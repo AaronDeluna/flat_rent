3f372f1cffe3c6100eeec97d08fb6999d1caf849
AaronDeluna <aaron_deluna@mail.ru>
2025-02-16 20:09:38 +0300

    Initial commit

---
+++ src/main/java/org/javaacademy/flat_rents/FlatRentsApplication.java
@@ -0,0 +1,13 @@
+package org.javaacademy.flat_rents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatRentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlatRentsApplication.class, args);
+    }
+
+}
